7a0e8c492b8513e548d70d8ba64d61e9bb5d9438
vuta <verso.930@gmail.com>
2017-06-21 16:50:35 +0200

    // Steps getall, getbyid, insert

--- src/main/java/com/worldrunner/dao/StepDao.java
+++ src/main/java/com/worldrunner/dao/StepDao.java
@@ -15,7 +15,7 @@ public interface StepDao {
 
     HashMap<User, HashMap<Integer, List<Integer>>> findById(int id, int page, int limit) throws  Exception;
     List<Step> findByDate(String datetime, int page, int limit) throws  Exception;
-    User insertStep(Step step) throws Exception;
-    User updateStep(Step step) throws  Exception;
+    Step insertStep(Step step) throws Exception;
+    Step updateStep(Step step) throws  Exception;
     void deleteStep(Step step) throws  Exception;
 }
